142f7d9d0934708681425064ec8e58be55b7458c
peterwof <peterwof@mit.edu>
2018-11-30 12:39:53 -0500

    modified ringlaunch.sh and added launchend.sh for killing sim

---
+++ launchend.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sleep 15
+cd ~/ZR_Astrobee/game_v_0.0/
+./gradlew --stop
+killall roslaunch
--- ringlaunch.sh
+++ ringlaunch.sh
@@ -3,6 +3,7 @@ sleep 3
 source ~/freeflyer_build/native/devel/setup.bash
 roslaunch astrobee spawn_ring.launch ns:=ring1 pose:="3.0 0.5 4.9 0.7071 0.0 0.0 0.7071"
 roslaunch astrobee spawn_ring.launch ns:=ring2 pose:="1.0 -0.5 4.9 -0.7071 0.0 0.0 0.7071"
-cd game_v_0.0
+cd ~/ZR_Astrobee/game_v_0.0
 ./gradlew build
+~/ZR_Astrobee/launchend.sh &
 ./gradlew run
